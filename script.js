b87075dbcfc19d57785b55580d78278999d113ec
bartr0 <bartlomiej.centkowski@zsmeie.edu.torun.pl>
2024-03-22 20:33:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,37 @@ form.addEventListener('submit', function(e) {
 });
 
 function getWeather(city) {
-    // Kod do pobrania danych o pogodzie za pomocą API
+    // Klucz API OpenWeatherMap
+    const apiKey = '8e703ab4af69c7dee4a34ad2423d86a2';
+
+    // Tworzymy URL zapytania do API
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    // Wysyłamy zapytanie HTTP GET za pomocą fetch
+    fetch(apiUrl)
+        .then(response => {
+            // Sprawdzamy, czy odpowiedź jest prawidłowa (status HTTP 200)
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            // Jeśli odpowiedź jest prawidłowa, przetwarzamy odpowiedź JSON
+            return response.json();
+        })
+        .then(data => {
+            // Przetwarzamy otrzymane dane o pogodzie
+            displayWeather(data);
+        })
+        .catch(error => {
+            // Obsługujemy błędy sieciowe lub błędy związane z przetwarzaniem danych
+            console.error('There was a problem with your fetch operation:', error);
+        });
 }
+
+function displayWeather(data) {
+    // Tutaj możemy wyświetlić dane o pogodzie na stronie
+    console.log(data);
+}
+
 
 function getCityPhoto(city) {
     // Kod do pobrania zdjęcia miasta za pomocą API
